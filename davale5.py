f7a6c5361317983e2ddb123a7af0045557b7b60e
mariamrevishvili <140517621+mariamrevishvili@users.noreply.github.com>
2023-07-30 01:22:30 +0400

    Add files via upload

---
+++ davale5.py
@@ -0,0 +1,9 @@
+#day 5 homework 
+saxeli= input ( " whats your name:  ")
+gvari= input ( " whats your surname: ")
+asaki= input ( " how old are you:  ")
+qveyana= input ( " which country do u live in: ")
+qalaqi= input (" which city do u live in: ")
+hobby= input (" whats your hobby: ")
+
+print( "gamarjoba" + " " + "me var " + saxeli + "  " + gvari + ", var "  + asaki + " wlis" + " vcxovrob " + qveyana + " kerdzod " + qalaqi + " miyvars " + hobby )
